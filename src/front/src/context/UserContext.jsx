219cfffd7899e9b1b76fd425755d4051bb192d40
Dani Ponce Mendoza <danniscreamer@gmail.com>
2023-04-12 09:57:39 -0600

    se mofificó el useState del UserContext, faltó instanciar el arreglo de objetos dentro

--- src/front/src/context/UserContext.jsx
+++ src/front/src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react'
 export const UserContext = createContext({});
 
 const UserProvider = ({children}) => {
-    const [userData,setUserData] = useState()
+    const [userData,setUserData] = useState({})
   return (
     <div>
       <UserContext.Provider value={{userData,setUserData}}>
